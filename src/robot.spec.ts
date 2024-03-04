c33ee1a5915ac873255e63e5056cb0d385a688fe
Tom Davis <tom.davis@rumr.co.uk>
2024-03-04 21:51:00 +0000

    Return valid position

--- src/robot.spec.ts
+++ src/robot.spec.ts
@@ -1,8 +1,26 @@
 import {Robot} from './robot';
 
 describe('Robot', () => {
-  test('new robot should return default position', () => {
-    const robot = new Robot();
+  let robot: Robot;
+
+  beforeEach(() => {
+    robot = new Robot();
+  });
+
+  it('should return default position', () => {
     expect(robot.getPosition()).toBe('0 0 N');
+  });
+
+  it.each([
+    'invalid'
+  ])('should throw error when setting position with invalid format', (position: string) => {
+    expect(() => {
+      robot.setPosition(position);
+    }).toThrow('Invalid position format.');
+  });
+
+  it('should return new position after being successfully set', () => {
+    robot.setPosition('1 1 N');
+    expect(robot.getPosition()).toBe('1 1 N');
   });
 });
--- src/robot.ts
+++ src/robot.ts
@@ -1,5 +1,21 @@
 export class Robot {
+  private position: string;
+
+  constructor() {
+    this.position = '0 0 N';
+  }
+
   public getPosition() {
-    return '0 0 N';
+    return this.position;
+  }
+
+  public setPosition(position: string) {
+    const parts = position.split(' ');
+
+    if (parts.length !== 3) {
+      throw new Error('Invalid position format.');
+    }
+
+    this.position = position;
   }
 }
