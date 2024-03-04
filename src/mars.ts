2fe755f13a5f54c2a3c423ea0297dd410afe5de1
Tom Davis <tom.davis@rumr.co.uk>
2024-03-04 18:22:31 +0000

    Validate minimum coordinate value

--- src/mars.ts
+++ src/mars.ts
@@ -3,7 +3,7 @@ export class Mars {
   private constructor(private x: number, private y: number) {}
 
   public static create(x: number, y: number) {
-    if (x > 50 || y > 50) {
+    if (x < 1 || x > 50 || y < 1 || y > 50) {
       throw new Error('Invalid coordinates. Valid coordinates range from 1 to 50.');
     }
 
