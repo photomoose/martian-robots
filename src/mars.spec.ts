4bdb67792f8c248bc732d7c80ceb348214be6820
Tom Davis <tom.davis@rumr.co.uk>
2024-03-04 18:20:43 +0000

    Failing tests for minimum coordinate values

--- src/mars.spec.ts
+++ src/mars.spec.ts
@@ -13,6 +13,18 @@ describe('Mars', () => {
     }).toThrow('Invalid coordinates. Valid coordinates range from 1 to 50.');
   });
 
+  it('creating Mars with x coordinate less than 1 should throw', () => {
+    expect(() => {
+      Mars.create(0, 1);
+    }).toThrow('Invalid coordinates. Valid coordinates range from 1 to 50.');
+  });
+
+  it('creating Mars with y coordinate less than 1 should throw', () => {
+    expect(() => {
+      Mars.create(1, 0);
+    }).toThrow('Invalid coordinates. Valid coordinates range from 1 to 50.');
+  });
+
   it('creating Mars with valid coordinates should return instance', () => {
     expect(Mars.create(3, 3)).toBeDefined();
   });
