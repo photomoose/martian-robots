a7b2d892473f761e5afff5c1c34f518a4ae32ee7
Tom Davis <tom.davis@rumr.co.uk>
2024-03-05 13:10:31 +0000

    Add examples to prompts

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const p = prompt({
   sigint: true
 });
 
-console.log('Enter upper-right coordinates of Mars');
+console.log('Enter upper-right coordinates of Mars (e.g. 5 3)');
 const upperRight = p('> ');
 
 const mars = Mars.create(upperRight);
@@ -17,12 +17,12 @@ let robotNumber = 1;
 // noinspection InfiniteLoopJS
 while (true) {
   try {
-    console.log(`Enter position of Robot ${robotNumber}`);
+    console.log(`Enter position of Robot ${robotNumber} (e.g. 1 1 N)`);
     const position = p('> ');
     const robot = new Robot(mars, [new Left(), new Right(), new Forward()]);
     robot.setPosition(position);
 
-    console.log(`Enter instructions to send to Robot ${robotNumber}`);
+    console.log(`Enter instructions to send to Robot ${robotNumber} (e.g. RFLF)`);
     const instructions = p('> ');
     robot.processInstructions(instructions);
 
